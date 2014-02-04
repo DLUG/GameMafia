82b9191e0dac6ca990943519b3b48f02cd6a5603
k <k@k-PC>
2014-02-04 14:40:55 +0900

    직업 소스추가

--- GameMafia/src/com/anyway/android/gamemafia/player/Mafia.java
+++ GameMafia/src/com/anyway/android/gamemafia/player/Mafia.java
@@ -14,4 +14,32 @@ public class Mafia extends Job {
 	public boolean isPossibleKillMyself() {
 		return killMyself;		
 	}
+//	public Mafia()
+//	{
+//		this.jobname =  "마피아";
+//		this.whichTeam = false;
+//		this.vote();
+//	}
+//	boolean killMySelf = false;
+//	int mafiaChoice[] = new int[8];
+//	public void shoot()
+//	{
+//		for(i = 0 ; i < 8 ; i++)
+//		{
+//			image[i].setOnClickListener(new ImageButton.OnClickListener() {
+//				public void onClick(View v)
+//				{
+//					for(int j = 0 ; j < playercount; j ++)
+//					{
+//						mafiaChoice[j] = 0;
+//						image[j].setImageDrawable(R.drawable."총안맞은이미지");
+//					}	
+//					mafiaChoice[i] = 1;
+//					image[i].setImageDrawable(R.drawable."총맞은이미지");
+//				}
+//			});
+//		}
+//	}
+	
+
 }
--- GameMafia/src/com/anyway/android/gamemafia/player/Police.java
+++ GameMafia/src/com/anyway/android/gamemafia/player/Police.java
@@ -1,9 +1,123 @@
 package com.anyway.android.gamemafia.player;
 
 public class Police extends Job {
+//	public Police()
+//	{
+//		this.jobname = "경찰";
+//		this.whichTeam = true;
+//		this.vote();
+//	}
+//	int policeChoice = 0;
+//	public void confirmation()
+//	{
+//		for(int i = 0; i < 8 ; i++)
+//		{
+//			image[i].setOnClickListener(new ImageButton.OnClickListener() {
+//				public void onClick(View v)
+//				{
+//					policeChoice = i;
+//					if(playNumber[policeChoice].job.isEqual("마피아"))
+//					{
+//						image[policeChoice].setImageDrawable(R.id."마피아인이미지");
+//					}
+//					break;
+//				}	
+//			});
+//		}
+//		image[0].setOnClickListener(new ImageButton.OnClickListener() {
+//			public void onClick(View v)
+//			{
+//				policeChoice = 0;
+//				if(playNumber[policeChoice].job.isEqual("마피아"))
+//				{
+//					image[policeChoice].setImageDrawable(R.id."마피아인이미지");
+//				}
+//				break;
+//			}	
+//		});
+//		image[1].setOnClickListener(new ImageButton.OnClickListener() {
+//			public void onClick(View v)
+//			{
+//				policeChoice = 1;
+//				if(playNumber[policeChoice].job.isEqual("마피아"))
+//				{
+//					image[policeChoice].setImageDrawable(R.id."마피아인이미지");
+//				}
+//				break;
+//			}	
+//		});
+//		image[2].setOnClickListener(new ImageButton.OnClickListener() {
+//			public void onClick(View v)
+//			{
+//				policeChoice = 2;
+//				if(playNumber[policeChoice].job.isEqual("마피아"))
+//				{
+//					image[policeChoice].setImageDrawable(R.id."마피아인이미지");
+//				}
+//				break;
+//			}	
+//		});
+//		image[3].setOnClickListener(new ImageButton.OnClickListener() {
+//			public void onClick(View v)
+//			{
+//				policeChoice = 3;
+//				if(playNumber[policeChoice].job.isEqual("마피아"))
+//				{
+//					image[policeChoice].setImageDrawable(R.id."마피아인이미지");
+//				}
+//				break;
+//			}	
+//		});
+//		image[4].setOnClickListener(new ImageButton.OnClickListener() {
+//			public void onClick(View v)
+//			{
+//				policeChoice = 4;
+//				if(playNumber[policeChoice].job.isEqual("마피아"))
+//				{
+//					image[policeChoice].setImageDrawable(R.id."마피아인이미지");
+//				}
+//				break;
+//			}	
+//		});
+//		image[5].setOnClickListener(new ImageButton.OnClickListener() {
+//			public void onClick(View v)
+//			{
+//				policeChoice = 5;
+//				if(playNumber[policeChoice].job.isEqual("마피아"))
+//				{
+//					image[policeChoice].setImageDrawable(R.id."마피아인이미지");
+//				}
+//				break;
+//			}	
+//		});
+//		image[6].setOnClickListener(new ImageButton.OnClickListener() {
+//			public void onClick(View v)
+//			{
+//				policeChoice = 6;
+//				if(playNumber[policeChoice].job.isEqual("마피아"))
+//				{
+//					image[policeChoice].setImageDrawable(R.id."마피아인이미지");
+//				}
+//				break;
+//			}	
+//		});
+//		image[7].setOnClickListener(new ImageButton.OnClickListener() {
+//			public void onClick(View v)
+//			{
+//				policeChoice = 7;
+//				if(playNumber[policeChoice].job.isEqual("마피아"))
+//				{
+//					image[policeChoice].setImageDrawable(R.id."마피아인이미지");
+//				}
+//				break;
+//			}	
+//		});
+//	}
+//		
 	
 	
 	public int confirmation() {
 		return 0;
+		
 	}
 }
