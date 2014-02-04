3240836ba403529d7f3456ea159875707e4a7216
roien <roien0402@gmail.com>
2014-02-05 01:41:28 +0900

    로그인은 DB 연동했다가 다시 DB 관련부분 제거하다가 코드가 꼬임...
    회원가입 버튼 누르면 해당 액티비티로 전환, 회원가입 페이지에서 취소를 누르면 다시 로그인 액티비티로 전환.
    
    로그인은 간단한 코드로 재정리해서 올리겠음.

--- GameMafia/src/com/anyway/android/gamemafia/JoinActivity.java
+++ GameMafia/src/com/anyway/android/gamemafia/JoinActivity.java
@@ -1,16 +1,30 @@
 package com.anyway.android.gamemafia;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JoinActivity extends Activity {
-
-	/** Called when the activity is first created. */
+	
+	private Button btn_cancel;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_join);
-	    // TODO Auto-generated method stub
+	    
+	    btn_cancel = (Button)findViewById(R.id.join_btn_cancel);
+	    // click btn_cancel button action
+	    btn_cancel.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 }
--- GameMafia/src/com/anyway/android/gamemafia/LoginActivity.java
+++ GameMafia/src/com/anyway/android/gamemafia/LoginActivity.java
@@ -2,14 +2,34 @@ package com.anyway.android.gamemafia;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
+	private EditText editTxt_id;
+	private EditText editTxt_pw;
+	private Button btn_login;
+	private Button btn_join;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		btn_join = (Button)findViewById(R.id.login_btn_join);
+		// click join button action
+		btn_join.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
