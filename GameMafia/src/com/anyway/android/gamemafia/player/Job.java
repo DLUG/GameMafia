9030b2499d8371f8c6fa3b116cc4e4fb37def8c3
k <k@k-PC>
2014-02-04 14:04:39 +0900

    일단 예전에 적어놨던 소스입니다.

--- GameMafia/src/com/anyway/android/gamemafia/player/Job.java
+++ GameMafia/src/com/anyway/android/gamemafia/player/Job.java
@@ -1,9 +1,35 @@
 package com.anyway.android.gamemafia.player;
 
 public abstract class Job {
-	private String jobName;		// 직업명
-	private boolean whichTeam;	// 시민팀 0, 마피아팀 1
+	private String jobName;		// 吏곸뾽紐�	private boolean whichTeam;	// �쒕���0, 留덊뵾�꾪� 1
 	private int vote() {
 		return 0;	// selectNumber
 	}
+	
+	
+//	private ImageButton image[] = new ImageButton[8];
+//	private byte voteValue[] = new byte[8];
+//	private int playercount = 0;
+//	private String jobname = ""; 
+//	private int index = 0;
+//	private boolean whichTeam = false; // false는 마피아팀
+//	public void vote()
+//	{
+//		for(index = 0 ; index < 8 ; index++)
+//		{
+//			image[index].setOnClickListener(new ImageButton.OnClickListener() {
+//				public void onClick(View v)
+//				{
+//					for(int j = 0 ; j < playercount; j ++)
+//					{
+//						voteValue[j] = 0;
+//						image[j].setImageDrawable(R.drawable."선택안된이미지");
+//					}	
+//					voteValue[index] = 1; //이거 왜 오류?
+//					image[index].setImageDrawable(R.drawable."선택된이미지");
+//				}
+//			});
+//		}
+//	}
+//	
 }
